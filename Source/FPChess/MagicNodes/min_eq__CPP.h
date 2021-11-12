35bac4f57249de19c88819b546088607e6fa2a57
Kenneth Stover <KStover@student.neumont.edu>
2021-11-12 15:42:25 -0700

    I DID A LOT OF SHIT

---
+++ Source/FPChess/MagicNodes/min_eq__CPP.h
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////
+///		{MGC} : GENERATED CLASS * Do NOT edit!
+////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Runtime/Core/Public/CoreMinimal.h"
+#include "Runtime/CoreUObject/Public/UObject/Object.h"
+#include "Runtime/CoreUObject/Public/UObject/ObjectMacros.h"
+#include "Runtime/CoreUObject/Public/UObject/WeakObjectPtr.h"
+#include "Runtime/CoreUObject/Public/UObject/SoftObjectPtr.h"
+#include "Runtime/CoreUObject/Public/UObject/SoftObjectPath.h"
+#include "Runtime/CoreUObject/Public/UObject/UObjectIterator.h"
+
+
+
+#include "MagicNodeRuntime.h"
+#include "min_eq__CPP.generated.h"
+
+
+
+
+
+
+
+
+UCLASS()
+class FPCHESS_API Umin_eq__CPP : public UMagicNode
+{
+	GENERATED_BODY()
+
+public:
+
+UFUNCTION (BlueprintCallable,Category="Magic Node",meta=(WorldContext="Context",BlueprintInternalUseOnly="true",ToolTip="Umin_eq__CPP"))
+ static void Execute(UObject* Context);
+
+
+
+
+
+
+
+
+};
+
+////////////////////////////////////////////////////////////////
